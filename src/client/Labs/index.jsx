d1aad821b3942925f7081bb8ad339fab678e26cc
kevinccbsg <kevinccbsg@gmail.com>
2018-01-15 19:55:23 +0100

    Añadida la petición de la home

--- src/client/Labs/index.jsx
+++ src/client/Labs/index.jsx
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
+import axios from 'axios';
 import LabItem from './LabItem';
 
-const Labs = () => (
-  <div className="inside-container">
-    <Header as="h1">User Profile</Header>
-    <div className="labs-container">
-      <LabItem />
-      <LabItem />
-      <LabItem />
-      <LabItem />
-    </div>
-  </div>
-);
+class Labs extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      items: [],
+    };
+  }
+
+  componentWillMount() {
+    this.setState({ loading: true });
+    axios.get('/GSITAE/labs')
+    .then((response) => {
+      const items = response.data.labs;
+      this.setState({ items, loading: false });
+    })
+    .catch(err => console.log(err.response));
+  }
+
+  render() {
+    const { loading, items } = this.state;
+    if (loading) {
+      return <p>Loading....</p>;
+    }
+    return (
+      <div className="inside-container">
+        <Header as="h1">GSITAE Labs</Header>
+        <div className="labs-container">
+          {items.map(obj => (
+            <LabItem
+              key={obj.name}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Labs;
