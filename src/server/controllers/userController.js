5817e1c12953d556be9e174a996cf1ae4bcc1bf6
kevinccbsg <kevinccbsg@gmail.com>
2018-02-08 00:19:29 +0100

    Refactorizadas las utilidades de usuarios

--- src/server/controllers/userController.js
+++ src/server/controllers/userController.js
@@ -3,6 +3,8 @@ import config from 'app-config';
 import _ from 'lodash';
 import logger from '../utils/logger';
 import response from '../utils/responseHelper';
+import clientHTTP from '../clientHTTP';
+import setToken from '../utils/setToken';
 
 const userFields = [
   'name',
@@ -13,38 +15,64 @@ const userFields = [
   'faculty',
 ];
 
+const client = clientHTTP(config.config.userapiOptions);
+
 const debug = require('debug')('GSITAE:userController');
 
 const userList = async (req, res) => {
   debug('[userController] userList');
-  return response(res, false, 'Not Implemented', 500);
-  /* try {
-    // const mongoResponse = await User.find({});
+  try {
+    const headers = setToken(req);
+    client.setHeaders(headers);
+    const apiResponse = await client.getRequest('/userapi/users');
     logger.info('[userController] User list information');
-    return response(res, true, { users: mongoResponse }, 200);
+    return response(res, true, { users: apiResponse.body }, 200);
   } catch (err) {
     debug('[userController] Error');
     debug(err);
     logger.error('[userController] Error User list information');
     return response(res, false, err, 500);
-  }*/
+  }
 };
 
 const createUser = async (req, res) => {
   debug('[userController] createUser');
-  return response(res, false, 'Not Implemented', 500);
-  /* try {
+  try {
     const payload = _.pick(req.body, userFields);
     const userPayload = { ...payload, roles: config.config.basicInitRole };
-    // const newUser = new User(userPayload);
-    await newUser.save();
+    const headers = setToken(req);
+    client.setHeaders(headers);
+    await client.postRequest('/userapi/user', userPayload);
     return response(res, true, payload, 201);
   } catch (err) {
     debug('[userController] Error');
     debug(err);
     logger.error('[userController] Error User list information');
     return response(res, false, err, 500);
-  }*/
+  }
+};
+
+const patchUser = async (req, res) => {
+  debug('[userController] patchUser');
+  const { code } = req.params;
+  if (_.isString(code)) {
+    debug('[userController] Error');
+    logger.error('[userController] Error deleting User. Bad request. identifier must be String');
+    return response(res, false, 'Bad Request', 400);
+  }
+  try {
+    await client.patchRequest(`/userapi/user/${code}`, req.body);
+    return response(res, false, req.body, 200);
+  } catch (err) {
+    debug('[userController] Error');
+    if (err.status === 404) {
+      logger.error('[userController] Error deleting User. Not Found');
+      return response(res, false, err.message, 404);
+    }
+    debug(err);
+    logger.error('[userController] Error deleting User');
+    return response(res, false, err, 500);
+  }
 };
 
 const deleteUser = async (req, res) => {
@@ -55,16 +83,8 @@ const deleteUser = async (req, res) => {
     logger.error('[userController] Error deleting User. Bad request. identifier must be String');
     return response(res, false, 'Bad Request', 400);
   }
-  return response(res, false, 'Not Implemented', 500);
-  /* try {
-    // const responseRemove = await User.remove({ code });
-    if (responseRemove.nRemoved === 0) {
-      const error = {
-        status: 404,
-        message: 'Not found register to delete',
-      };
-      throw error;
-    }
+  try {
+    await client.deleteRequest(`/userapi/user/${code}`);
     return response(res, false, 'User removed', 204);
   } catch (err) {
     debug('[userController] Error');
@@ -75,7 +95,7 @@ const deleteUser = async (req, res) => {
     debug(err);
     logger.error('[userController] Error deleting User');
     return response(res, false, err, 500);
-  }*/
+  }
 };
 
 const getUser = async (req, res) => {
@@ -86,17 +106,9 @@ const getUser = async (req, res) => {
     logger.error('[userController] Error getting User. Bad request. identifier must be Number');
     return response(res, false, 'Bad Request', 400);
   }
-  return response(res, false, 'Not Implemented', 500);
-  /* try {
-    const responseUser = await User.findOne({ code });
-    if (!responseUser) {
-      const error = {
-        status: 404,
-        message: 'Not found register to delete',
-      };
-      throw error;
-    }
-    return response(res, false, responseUser, 200);
+  try {
+    const apiResponse = await client.deleteRequest(`/userapi/user/${code}`);
+    return response(res, false, apiResponse.body, 200);
   } catch (err) {
     debug('[userController] Error');
     if (err.status === 404) {
@@ -106,108 +118,29 @@ const getUser = async (req, res) => {
     debug(err);
     logger.error('[userController] Error deleting User');
     return response(res, false, err, 500);
-  }*/
+  }
 };
 
-const addRolePermission = async (req, res) => {
-  debug('[userController] addRolePermission');
-  const { code } = req.params;
-  const property = (req.originalUrl.indexOf('role') !== -1) ? 'roles' : 'permissions';
-  if (_.isString(code)) {
-    debug('[userController] Error');
-    logger.error('[userController] Error adding Role User. Bad request. identifier must be Number');
-    return response(res, false, 'Bad Request', 400);
-  }
-  return response(res, false, 'Not Implemented', 500);
-  /* try {
-    const responseUser = await User.update({ code }, {
-      $push: {
-        [property]: req.body,
-      },
-    });
-    if (responseUser.nModified === 0) {
-      const error = {
-        status: 404,
-        message: 'Not found register to update',
-      };
-      throw error;
-    }
-    return response(res, false, `Updating ${property}`, 200);
-  } catch (err) {
-    debug('[userController] Error');
-    if (err.status === 404) {
-      logger.error('[userController] Error updating User. Not Found');
-      return response(res, false, err.message, 404);
-    }
-    debug(err);
-    logger.error('[userController] Error updating User');
-    return response(res, false, err, 500);
-  }*/
-};
-
-const removeRolePermission = async (req, res) => {
-  debug('[userController] removeRolePermission');
-  const { code } = req.params;
-  const property = (req.originalUrl.indexOf('role') !== -1) ? 'roles' : 'permissions';
-  if (_.isString(code)) {
-    debug('[userController] Error');
-    logger.error(`[userController] Error adding ${property} User. Bad request. identifier must be Number`);
-    return response(res, false, 'Bad Request', 400);
-  }
-  return response(res, false, 'Not Implemented', 500);
-  /* try {
-    const responseUser = await User.update({ code }, {
-      $pull: {
-        [property]: req.body,
-      },
-    });
-    if (responseUser.nModified === 0) {
-      const error = {
-        status: 404,
-        message: 'Not found register to update',
-      };
-      throw error;
-    }
-    return response(res, false, `Removed ${property}`, 204);
-  } catch (err) {
-    debug('[userController] Error');
-    if (err.status === 404) {
-      logger.error('[userController] Error updating User. Not Found');
-      return response(res, false, err.message, 404);
-    }
-    debug(err);
-    logger.error('[userController] Error updating User');
-    return response(res, false, err, 500);
-  }*/
-};
 
 const getRolePermissions = async (req, res) => {
   debug('[userController] getRolePermissions');
-  return response(res, false, 'Not Implemented', 500);
-  /* try {
-    const mongoResponse = await Promise.all([
-      Permission.find({}),
-      Roles.find({}),
-    ]);
+  try {
+    const apiResponse = await client.getRequest('/userapi/rolepermission');
     logger.info('[userController] User list information');
-    return response(res, true, {
-      roles: mongoResponse[1],
-      permissions: mongoResponse[0],
-    }, 200);
+    return response(res, true, apiResponse.body, 200);
   } catch (err) {
     debug('[userController] Error');
     debug(err);
     logger.error('[userController] Error User list information');
     return response(res, false, err, 500);
-  }*/
+  }
 };
 
 export {
-  removeRolePermission,
-  addRolePermission,
   getUser,
   deleteUser,
   createUser,
   userList,
   getRolePermissions,
+  patchUser,
 };
--- src/server/router/index.js
+++ src/server/router/index.js
@@ -4,9 +4,8 @@ import {
   userList,
   createUser,
   getUser,
-  removeRolePermission,
-  addRolePermission,
   deleteUser,
+  patchUser,
   getRolePermissions,
 } from './../controllers/userController';
 import {
@@ -33,10 +32,7 @@ router.delete('/lab/:nameLab', deleteLab);
 
 router.get('/users', userList);
 router.post('/user', createUser);
-router.post('/user/:code/role', addRolePermission);
-router.post('/user/:code/permission', addRolePermission);
-router.delete('/user/:code/role', removeRolePermission);
-router.delete('/user/:code/permission', removeRolePermission);
+router.patch('/user/:code', patchUser);
 router.delete('/user/:code', deleteUser);
 router.get('/user/:code', getUser);
 
