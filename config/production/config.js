75756de5d60b04b93f4bcc08ed48889e16409c0e
kevinccbsg <kevinccbsg@gmail.com>
2018-04-03 00:09:00 +0200

    Añadida la configuración de producción

---
+++ config/production/config.js
@@ -0,0 +1,73 @@
+const {
+  provision_key_userapi,
+  client_id_application,
+  id_application,
+  client_secret_application,
+  consumer_id_application,
+} = require('./../secrets');
+
+module.exports = {
+  port: 3000,
+  ldap_OPTS: {
+    server: {
+      url: 'ldap://192.168.1.91:10389',
+      searchBase: 'ou=users,dc=example,dc=com',
+      searchFilter: '(cn={{username}})',
+      reconnect: true,
+    },
+  },
+  redis: {
+    host: 'redis-dashboard',
+    port: 6379,
+    ttl: 1 * 12 * 60 * 60,
+  },
+  ruleTokens: '*/30 * * * *',
+  userapi: {
+    provision_key: provision_key_userapi,
+  },
+  application: {
+    client_id: client_id_application,
+    created_at: 1516053687932,
+    id: id_application,
+    name: 'Test-Application',
+    client_secret: client_secret_application,
+    consumer_id: consumer_id_application,
+  },
+  kongOptions: {
+    protocol: 'http',
+    ip: 'kong',
+    port: 8001,
+    strictSSL: false,
+  },
+  userapiOptions: {
+    protocol: 'https',
+    ip: 'kong',
+    port: 8443,
+    strictSSL: false,
+  },
+  kongOauth2PluginOptions: {
+    protocol: 'https',
+    ip: 'kong',
+    port: 8443,
+    strictSSL: false,
+  },
+  kongOauth2Plugin: {
+    name: 'oauth2',
+    'config.enable_authorization_code': true,
+    'config.mandatory_scope': false,
+    'config.enable_client_credentials': true,
+  },
+  basicInitRole: [
+    {
+      name: 'User',
+      description: 'user.role',
+    },
+  ],
+  session: {
+    secret: 'stta azrisk tcv',
+    cookie: { maxAge: 7200000 },
+    resave: false,
+    saveUninitialized: false,
+  },
+  adminRole: 'Admin',
+};
